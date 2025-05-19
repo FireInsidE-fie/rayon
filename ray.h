16b66a656b1652bdc9787e4c3a180e74c2352fe2
Aisling Fontaine <aisling.fontaine@protonmail.com>
2025-05-19 14:00:49 +0200

    blue to white gradient on the y coordinate

--- ray.h
+++ ray.h
@@ -13,6 +13,9 @@ public:
 	const point3& origin() const { return orig; }
 	const vec3& direction() const { return dir; }
 
+	/**
+	 * @brief Find the point along the current ray indicated by variable `t`.
+	 */
 	point3 at(double t) const
 	{
 		return orig + t * dir;
