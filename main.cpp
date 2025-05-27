79f21db174cdb5c408e25c5d11911a81288c69db
Aisling Fontaine <aisling.fontaine@protonmail.com>
2025-05-27 18:14:39 +0200

    make a more interesting scene

--- main.cpp
+++ main.cpp
@@ -10,7 +10,9 @@ int main()
 	hittable_list world;
 
 	world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
-	world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
+	world.add(make_shared<sphere>(point3(1, 0, -1), 0.5));
+	world.add(make_shared<sphere>(point3(-1, 0, -1), 0.5));
+	world.add(make_shared<sphere>(point3(0, -10.5, -1), 10));
 
 	camera cam;
 
